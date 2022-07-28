de3e8854245c4fe44c8334626252fb8137f99ce7
Alex Cameron <asc@tetsuo.sh>
2022-07-28 23:32:15 +1000

    Add verification logic

--- action.py
+++ action.py
@@ -35,8 +35,12 @@ def _log(msg):
         print(msg, file=output)
 
 
-def _sigstore_python(*args):
+def _sigstore_python_sign(*args):
     return ["python", "-m", "sigstore", "sign", *args]
+
+
+def _sigstore_python_verify(*args):
+    return ["python", "-m", "sigstore", "verify", *args]
 
 
 def _fatal_help(msg):
@@ -47,8 +51,9 @@ def _fatal_help(msg):
 inputs = sys.argv[1].split()
 summary = Path(os.getenv("GITHUB_STEP_SUMMARY")).open("a")
 
-# The arguments we pass into `sigstore-python` get built up in this list.
-sigstore_python_args = []
+# The arguments we pass into `sigstore-python` get built up in these lists.
+sigstore_python_sign_args = []
+sigstore_python_verify_args = []
 
 # The environment variables that we apply to `sigstore-python`.
 sigstore_python_env = {}
@@ -58,48 +63,52 @@ if _DEBUG:
 
 client_id = os.getenv("GHA_SIGSTORE_PYTHON_OIDC_CLIENT_ID")
 if client_id != "":
-    sigstore_python_args.extend(["--oidc-client-id", client_id])
+    sigstore_python_sign_args.extend(["--oidc-client-id", client_id])
 
 client_secret = os.getenv("GHA_SIGSTORE_PYTHON_OIDC_CLIENT_SECRET")
 if client_secret != "":
-    sigstore_python_args.extend(["--oidc-client-secret", client_secret])
+    sigstore_python_sign_args.extend(["--oidc-client-secret", client_secret])
 
 if os.getenv("GHA_SIGSTORE_PYTHON_NO_DEFAULT_FILES", "false") != "false":
-    sigstore_python_args.append("--no-default-files")
+    sigstore_python_sign_args.append("--no-default-files")
 
 output_signature = os.getenv("GHA_SIGSTORE_PYTHON_OUTPUT_SIGNATURE")
 if output_signature != "":
-    sigstore_python_args.extend(["--output-signature", output_signature])
+    sigstore_python_sign_args.extend(["--signature", output_signature])
+    sigstore_python_verify_args.extend(["--signature", output_signature])
 
 output_certificate = os.getenv("GHA_SIGSTORE_PYTHON_OUTPUT_CERTIFICATE")
 if output_certificate != "":
-    sigstore_python_args.extend(["--output-certificate", output_certificate])
+    sigstore_python_sign_args.extend(["--certificate", output_certificate])
+    sigstore_python_verify_args.extend(["--certificate", output_certificate])
 
 if os.getenv("GHA_SIGSTORE_PYTHON_OVERWRITE", "false") != "false":
-    sigstore_python_args.append("--overwrite")
+    sigstore_python_sign_args.append("--overwrite")
 
 fulcio_url = os.getenv("GHA_SIGSTORE_PYTHON_FULCIO_URL")
 if fulcio_url != "":
-    sigstore_python_args.extend(["--fulcio-url", fulcio_url])
+    sigstore_python_sign_args.extend(["--fulcio-url", fulcio_url])
 
 rekor_url = os.getenv("GHA_SIGSTORE_PYTHON_REKOR_URL")
 if rekor_url != "":
-    sigstore_python_args.extend(["--rekor-url", rekor_url])
+    sigstore_python_sign_args.extend(["--rekor-url", rekor_url])
+    sigstore_python_verify_args.extend(["--rekor-url", rekor_url])
 
 ctfe = os.getenv("GHA_SIGSTORE_PYTHON_CTFE")
 if ctfe != "":
-    sigstore_python_args.extend(["--ctfe", ctfe])
+    sigstore_python_sign_args.extend(["--ctfe", ctfe])
 
 rekor_root_pubkey = os.getenv("GHA_SIGSTORE_PYTHON_REKOR_ROOT_PUBKEY")
 if rekor_root_pubkey != "":
-    sigstore_python_args.extend(["--rekor-root-pubkey", rekor_root_pubkey])
+    sigstore_python_sign_args.extend(["--rekor-root-pubkey", rekor_root_pubkey])
 
 oidc_issuer = os.getenv("GHA_SIGSTORE_PYTHON_OIDC_ISSUER")
 if oidc_issuer != "":
-    sigstore_python_args.extend(["--oidc-issuer", oidc_issuer])
+    sigstore_python_sign_args.extend(["--oidc-issuer", oidc_issuer])
 
 if os.getenv("GHA_SIGSTORE_PYTHON_STAGING", "false") != "false":
-    sigstore_python_args.append("--staging")
+    sigstore_python_sign_args.append("--staging")
+    sigstore_python_verify_args.append("--staging")
 
 for input_ in inputs:
     # Forbid things that look like flags. This isn't a security boundary; just
@@ -113,41 +122,83 @@ for input_ in inputs:
         if not file_.is_file():
             _fatal_help(f"input {file_} does not look like a file")
 
-    sigstore_python_args.extend(files)
+    sigstore_python_sign_args.extend(files)
+    sigstore_python_verify_args.extend(files)
 
-_debug(f"running: sigstore-python {[str(a) for a in sigstore_python_args]}")
+_debug(f"running: sigstore-python {[str(a) for a in sigstore_python_sign_args]}")
 
-status = subprocess.run(
-    _sigstore_python(*sigstore_python_args),
+sign_status = subprocess.run(
+    _sigstore_python_sign(*sigstore_python_sign_args),
     text=True,
     stdout=subprocess.PIPE,
     stderr=subprocess.STDOUT,
     env={**os.environ, **sigstore_python_env},
 )
 
-_debug(status.stdout)
+_debug(sign_status.stdout)
 
-if status.returncode == 0:
-    _log("🎉 sigstore-python exited successfully")
+if sign_status.returncode == 0:
+    _log("🎉 sigstore-python signing exited successfully")
 else:
     _log("❌ sigstore-python failed to sign package")
+
+verify_status = None
+if sign_status.return_code == 0:
+    verify_status = subprocess.run(
+        _sigstore_python_verify(*sigstore_python_verify_args),
+        text=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        env={**os.environ, **sigstore_python_env},
+    )
+    _debug(verify_status.stdout)
+
+if verify_status is None:
+    _log("❌ sigstore-python verification skipped due to failed signing")
+elif verify_status.returncode == 0:
+    _log("🎉 sigstore-python verification exited successfully")
+else:
+    _log("❌ sigstore-python failed to verify package")
+
 
 _summary(
     """
 <details>
 <summary>
-    Raw `sigstore-python` output
+    Raw `sigstore-python sign` output
 </summary>
 
 ```
     """
 )
-_log(status.stdout)
+_log(sign_status.stdout)
 _summary(
     """
 ```
+    """
+)
+if verify_status is not None:
+    _summary(
+        """
+<summary>
+    Raw `sigstore-python verify` output
+</summary>
+        """
+    )
+    _log(verify_status.stdout)
+    _summary(
+        """
+```
+        """
+    )
+_summary(
+    """
 </details>
     """
 )
 
-sys.exit(status.returncode)
+if sign_status.returncode != 0:
+    assert verify_status is None
+    sys.exit(sign_status.returncode)
+else:
+    sys.exit(verify_status.returncode)
